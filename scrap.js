be460a76ded37c811260986fa677e46c0e9b61c7
Angela1015 <49008406+Angela1015@users.noreply.github.com>
2019-11-17 11:37:31 -0600

    got checkoff and delete to work

--- scrap.js
+++ scrap.js
@@ -43,9 +43,16 @@ list.addEventListener('click', event => {
   if (event.target.classList.contains('js-tick')) {
     const itemKey = event.target.parentElement.dataset.key;
     toggleDone(itemKey);
+  } if(event.target.classList.contains('.js-delete-todo')){
+    const itemKey = event.target.parentElement.dataset.key;
+    deleteTodo(itemKey);
   }
+
 });
-});
+
+ 
+})
+
 function toggleDone(key) {
     const index = todoItems.findIndex(item => item.id === Number(key));
     todoItems[index].checked = !todoItems[index].checked;
@@ -58,3 +65,9 @@ function toggleDone(key) {
     }
   }
   
+  function deleteTodo(key){
+    todoItems = todoItems.filter(item=>item.id!==Number(key));
+    const item = document.querySelector(`[data-key='${key}']`);
+    item.remove();
+  }
+
